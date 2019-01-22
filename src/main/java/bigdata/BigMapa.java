5e020bed9f17f0dce90c7ed914a0fcfb0f4456d9
kfontain <fontaken@gmail.com>
2019-01-22 11:06:06 +0100

    Refactoring

--- src/main/java/bigdata/BigMapa.java
+++ src/main/java/bigdata/BigMapa.java
@@ -23,47 +23,9 @@ public class BigMapa {
 
         JavaPairRDD<String, PortableDataStream> rdd = context.binaryFiles("hdfs://young:9000/user/raw_data/dem3/N44W002.hgt");
         JavaPairRDD<String, short[]> rddLatLong = tileOperations.extractHeightLatLong(rdd);
+        tileOperations.generateTiles(rddLatLong);
 
         Tuple2<String, short[]> arcachon = rddLatLong.first();
         System.out.println(arcachon._1);
-
-        rddHeights.foreach(heights -> {
-            // Bytes per pixel = 3;
-            // Width/Height = 1201;
-            ByteBuffer buffer = ByteBuffer.allocate(3 * 1201 * 1201);
-            for (short h : heights) {
-                switch (h) {
-                    case 0:
-                        buffer.put(new byte[]{(byte) 0, (byte) 128, (byte) 128});
-                        break;
-                    default:
-                        buffer.put(new byte[]{(byte) h, (byte) 0, (byte) 0});
-                        break;
-                }
-            }
-            buffer.rewind();
-            DataBufferByte dbb = new DataBufferByte(buffer.array(), buffer.capacity());
-            WritableRaster raster = Raster.createInterleavedRaster(
-                    dbb,
-                    1201, 1201,
-                    1201 * 3,
-                    3,
-                    new int[]{0, 1, 2},
-                    null);
-
-            ColorModel colorModel = new ComponentColorModel(
-                    ColorSpace.getInstance(ColorSpace.CS_sRGB),
-                    new int[]{8, 8, 8},
-                    false,
-                    false,
-                    Transparency.OPAQUE,
-                    DataBuffer.TYPE_BYTE);
-
-            BufferedImage bfImage = new BufferedImage(colorModel, raster, false, null);
-            bfImage = (bfImage, 256, 256);
-
-            ImageIO.write(bfImage, "png", new File("output.png"));
-
-        });
     }
 }
