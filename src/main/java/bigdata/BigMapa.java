998fe6b8b1d7d501ba8d978e0c4cd902c8d6f187
kfontain <fontaken@gmail.com>
2019-01-22 10:53:36 +0100

    Add image generation

--- src/main/java/bigdata/BigMapa.java
+++ src/main/java/bigdata/BigMapa.java
@@ -6,6 +6,13 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.input.PortableDataStream;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.color.ColorSpace;
+import java.awt.image.*;
+import java.io.File;
+import java.nio.ByteBuffer;
+
 public class BigMapa {
 
     public static void main (String[] args) {
@@ -34,10 +41,40 @@ public class BigMapa {
             return height;
         });
 
-        rddHeights.foreach(ints -> {
-            for(int i = 0; i < ints.length; ++i) {
-                System.out.println(ints[i]);
+        rddHeights.foreach(heights -> {
+            // Bytes per pixel = 3;
+            // Width/Height = 1201;
+            ByteBuffer buffer = ByteBuffer.allocate(3 * 1201 * 1201);
+            for (short h : heights) {
+                switch(h) {
+                    case 0 : buffer.put(new byte[] {(byte) 0, (byte) 128, (byte) 128}); break;
+                    default : buffer.put(new byte[] {(byte) h, (byte) 0, (byte) 0}); break;
+                }
             }
+            buffer.rewind();
+            DataBufferByte dbb = new DataBufferByte(buffer.array(), buffer.capacity());
+            WritableRaster raster = Raster.createInterleavedRaster(
+                    dbb,
+                    1201, 1201,
+                    1201 * 3,
+                    3,
+                    new int []{ 0, 1, 2 },
+                    null);
+
+            ColorModel colorModel =  new ComponentColorModel(
+                    ColorSpace.getInstance(ColorSpace.CS_sRGB),
+                    new int[] { 8, 8, 8 },
+                    false,
+                    false,
+                    Transparency.OPAQUE,
+                    DataBuffer.TYPE_BYTE);
+
+            BufferedImage bfImage = new BufferedImage(colorModel, raster, false, null);
+            bfImage = (bfImage, 256, 256);
+
+            ImageIO.write(bfImage, "png", new File("output.png"));
+
         });
+
     }
 }
