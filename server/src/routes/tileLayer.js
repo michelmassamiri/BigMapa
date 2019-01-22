903c37c9bddcd6e4be16b94caecc4af18939a970
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2019-01-22 16:11:26 +0100

    Testing displaying from LeafLet an image stored in the local

--- server/src/routes/tileLayer.js
+++ server/src/routes/tileLayer.js
@@ -11,17 +11,37 @@ const client = new hbase.Client({
 const tileRow = new hbase.Row(client, 'test', '1') 
 */
 
+let val
+
+let options = {
+    root: __dirname + '../../../public/img',
+   // root: 'http://hbase.apache.org/images/',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+}
+
+const fileName = 'logo.png'
+
 hbase()
 .table('test')
 .row('1')
 .get('data:img', (error, value) => {
-    console.info(value)
+    val = value[0].$
+    console.info(val)
 })
 
 
-router.get('/api.tiles/:z/:x/:y', (req, res) => {
-    res.send(`You have requested a tile with a zoom level [${req.params.z}] and coordinates (${req.params.x}, ${req.params.y})`)
+router.get('/api.tiles/:z/:x/:y', (req, res, next) => {
+    res.sendFile(fileName, options, (err) => {
+        if (err) {
+            next(err)
+        } else {
+            console.log('Sent: ', fileName)
+        }
+    })
+    // res.send(`<h1>You have requested a tile with a zoom level [${req.params.z}] and coordinates (${req.params.x}, ${req.params.y}) \n <img src = "${val}"> </h1>`)
 })
-
 
 module.exports = router
