0549244f08c7cf05c4d124c1c73fedc5829e25a4
Serigne Amsatou Seye <serigne-amsatou.seye@etu.u-bordeaux.fr>
2019-01-25 17:02:18 +0100

    Getting image from HBase depending on the parameters of the GET request

--- server/src/routes/tileLayer.js
+++ server/src/routes/tileLayer.js
@@ -12,7 +12,6 @@ const client = new hbase.Client({
 const tileRow = new hbase.Row(client, 'test', '1') 
 */
 
-let val
 
 let options = {
     root: __dirname + '../../../public/',
@@ -24,46 +23,53 @@ let options = {
 
 const fileName = 'public/img.png'
 
-hbase()
-.table('BigMapa')
-.row('row1')
-.get('data:img', (error, value) => {
-    val = value[0].$
-    // console.info(val)
-})
+
 
 
 router.get('/api.tiles/:x/:y/:z', (req, res, next) => {
     if (req.params.x && req.params.y && req.params.z) {
-        let data = hexa2img(val)
-        fs.writeFile(fileName, data, (err) => {
-            if (err) throw err;
-            console.log('The file has been saved!');
-            res.sendFile(`img.png`, options, (err2) => {
-                if (err2) {
-                    next(err2)
-                } else {
-                    console.log('Sent: ', fileName)
-                }
-            })
-            res.on('finish', function () {
-                // Check if the file exists in the current directory.
-                if (!fs.existsSync(fileName)) {
-                    console.log(fileName, 'doesn\'t exist')
-                } 
-                else {
-                    // Have to remove setTimout !!!!
-                    setTimeout(function () {
-                        try {
-                            fs.unlinkSync(fileName);
-                            console.log(fileName, 'was deleted');
-                        } catch (e) {
-                            console.log("error removing ", fileName);
+        let rowID = req.params.x +','+ req.params.y
+        console.log(rowID);
+        
+        let val
+        hbase()
+            .table('michelmassamiri')
+            .row(rowID)
+            .get('data:image', (error, value) => {
+                val = value[0].$
+                // console.info(val)
+                /*val = "ahamz"
+                send(`<img src="data:image/png;base64,"+${val}`)*/
+                let data = hexa2img(val)
+                fs.writeFile(fileName, data, (err) => {
+                    if (err) throw err;
+                    console.log('The file has been saved!');
+                    res.sendFile(`img.png`, options, (err2) => {
+                        if (err2) {
+                            next(err2)
+                        } else {
+                            console.log('Sent: ', fileName)
                         }
-                    }, 000)
-                }
-            });
-        });
+                    })
+                    res.on('finish', function () {
+                        // Check if the file exists in the current directory.
+                        if (!fs.existsSync(fileName)) {
+                            console.log(fileName, 'doesn\'t exist')
+                        } 
+                        else {
+                            // Have to remove setTimout !!!!
+                            setTimeout(function () {
+                                try {
+                                    fs.unlinkSync(fileName);
+                                    console.log(fileName, 'was deleted');
+                                } catch (e) {
+                                    console.log("error removing ", fileName);
+                                }
+                            }, 000)
+                        }
+                    }); //res.on(...)
+                }); // fs.writeFile(...) 
+            }) // .get(...)
     }
 })
 
@@ -80,15 +86,15 @@ function clean_hex(rowData) {
 function hexa2img(hexa) {
     var cleaned_hex = clean_hex(hexa);
 
-    if (cleaned_hex.length % 2) {
-        alert("Error: cleaned hex string length is odd.");
-        return;
-    }
+    // if (cleaned_hex.length % 2) {
+       // alert("Error: cleaned hex string length is odd.");
+       // return;
+    // }
 
     var binary = new Array();
     for (var i = 0; i < cleaned_hex.length / 2; i++) {
         var h = cleaned_hex.substr(i * 2, 2);
-        binary[i] = parseInt(h, 16);
+        binary[i] = parseInt(h, 24);
     }
 
     var byteArray = new Uint8Array(binary);
