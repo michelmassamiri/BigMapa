d216ffbe58fdf85c5a713857504c1b88fa09b55c
michelmassamiri <massamiri.michel@gmail.com>
2019-01-27 12:48:33 +0100

    Try NodeJS for Hbase

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,13 @@
-let express = require('express')
-let app = express()
+let express = require('express');
+let app = express();
 
-let tileLayerRoute = require('./routes/tileLayer')
+let imageReadeRoute = require('./routes/readImage');
 
-app.use(tileLayerRoute)
+app.use(imageReadeRoute);
 
 app.use((req, res, next) => {
     res.status(404).send('Error 404: We can not find the requested resource !')
-})
+});
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.info(`Server listen on port ${PORT}...`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.info(`Server listen on port ${PORT}...`));
