128b5995fc8364525f607bfea92690b3a9473bb3
Serigne Amsatou SEYE <serigne-amsatou.seye@etu.u-bordeaux.fr>
2019-01-31 01:29:46 +0100

    Adding z request parameter for the rowID and changing the name of the table

--- server/src/routes/tileLayer.js
+++ server/src/routes/tileLayer.js
@@ -2,7 +2,7 @@ let express = require('express')
 let path = require('path')
 let router = express.Router()
 
-let app =  express()
+let app = express()
 
 const hbase = require('hbase')
 
@@ -10,18 +10,17 @@ app.use(express.static('public'))
 
 router.get('/api.tiles/:z/:x/:y', (req, res, next) => {
     if (req.params.x && req.params.y && req.params.z) {
-        let rowID = req.params.x +','+ (180-Number(req.params.y));
+        let rowID = req.params.x + ',' + (180 - Number(req.params.y)) + ',' + req.params.z;
         console.log(rowID);
-        
-        let val
+
+        let val;
         hbase()
-            .table('micheldomexic')
+            .table('BigMapa')
             .row(rowID)
-            .get('data:image', (error, value) => {
+            .get('data:image', (error, dataue) => {
                 if (error) {
                     res.sendFile(path.join(__dirname, '../../public/default.png'))
-                }
-                else {
+                } else {
                     val = value[0].$
                     let data = new Buffer(val, 'base64')
                     res.contentType('image/png');
